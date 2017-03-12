e17555dc7d18735296146f6e156717fe186bba83
Phoenix-VAIO\Suzaku <magdalena.jedzierowska@gmail.com>
2017-03-12 12:50:00 +0100

    Project update

--- TAJ_Projekt/4inRow/src/main/java/com/four/app/inRow/ConnectFour.java
+++ TAJ_Projekt/4inRow/src/main/java/com/four/app/inRow/ConnectFour.java
@@ -4,10 +4,9 @@ package com.four.app.inRow;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
+
+public class ConnectFour {
+	public static void main( String[] args ) {
         System.out.println( "Hello World!" );
     }
 }
