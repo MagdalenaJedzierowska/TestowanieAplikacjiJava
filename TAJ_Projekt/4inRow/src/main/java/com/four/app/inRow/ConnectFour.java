fc49ee2f9708f722e0842716027f3f8a8813d15d
Phoenix-VAIO\Suzaku <magdalena.jedzierowska@gmail.com>
2017-03-12 19:45:36 +0100

    Update

--- TAJ_Projekt/4inRow/src/main/java/com/four/app/inRow/ConnectFour.java
+++ TAJ_Projekt/4inRow/src/main/java/com/four/app/inRow/ConnectFour.java
@@ -1,5 +1,6 @@
 package com.four.app.inRow;
 
+import java.io.PrintStream;
 import java.util.Arrays;
 import java.util.stream.IntStream;
 
@@ -8,8 +9,12 @@ public class ConnectFour {
 	private static final int rows = 6;
 	private static final int columns = 7;
 	private static final String empty = " ";
+	private static final String RED = "R";
+	
+	private PrintStream outputChannel;
 
 	private String[][] board = new String[rows][columns];
+	private String currentPlayer = RED;
 
 	public ConnectFour(){
 		for(String[] row : board){
@@ -25,5 +30,32 @@ public class ConnectFour {
         return (int) IntStream.range(0, rows)
                 .filter(row -> !empty.equals(board[row][column]))
                 .count();
+    }
+    
+    public int putDisc(int column) {
+        checkColumn(column);
+        int row = getDiscInColNum(column);
+        checkPositionToPut(row, column);
+        board[row][column] = currentPlayer;
+        printBoard();
+        checkWinner(row, column);
+        switchPlayer();
+        return row;
+    }
+    
+    private void checkColumn(int column) {
+        if (column < 0 || column >= columns){
+            throw new RuntimeException(String.format("Invalid column %d", column));
+        }
+    }
+    
+    private void checkPositionToPut(int row, int column) {
+        if (row == rows)
+            throw new RuntimeException(String.format("No more room in column %d", column));
+    }
+    
+    public String getCurrentPlayer() {
+        outputChannel.printf("Player %s turn%n", currentPlayer);
+        return currentPlayer;
     }
 }
--- TAJ_Projekt/4inRow/src/test/java/com/four/app/inRow/ConnectFourTest.java
+++ TAJ_Projekt/4inRow/src/test/java/com/four/app/inRow/ConnectFourTest.java
@@ -100,7 +100,7 @@ public class ConnectFourTest {
     }
     
     @Test
-    public void 4inColWinnerTest() {
+    public void fourInColWinnerTest() {
         for (int row = 0; row < 3; row++) {
             tested.putDisc(1);
             tested.putDisc(2);
@@ -108,5 +108,35 @@ public class ConnectFourTest {
         assertThat(tested.getWinner(), isEmptyString());
         tested.putDisc(1);
         assertThat(tested.getWinner(), is("R"));
+    }
+    
+    @Test
+    public void fourInRowWinnerTest() {
+        int column;
+        for (column = 0; column < 3; column++) {
+            tested.putDisc(column);
+            tested.putDisc(column);
+        }
+        assertThat(tested.getWinner(), isEmptyString());
+        tested.putDisc(column);
+        assertThat(tested.getWinner(), is("R"));
+    }
+    
+    @Test
+    public void fourInSlashWinnerTest() {
+        int[] gameplay = new int[] {1, 2, 2, 3, 4, 3, 3, 4, 4, 5, 4};
+        for (int column : gameplay) {
+            tested.putDisc(column);
+        }
+        assertThat(tested.getWinner(), is("R"));
+    }
+    
+    @Test
+    public void fourInBackSlashWinnerTest() {
+        int[] gameplay = new int[] {3, 4, 2, 3, 2, 2, 1, 1, 1, 1};
+        for (int column : gameplay) {
+            tested.putDisc(column);
+        }
+        assertThat(tested.getWinner(), is("G"));
     }
 }
